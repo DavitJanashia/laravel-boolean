210e30d75054ed5355fdcb72ddfa85353593ff05
Davit <davit.janashia1@gmail.com>
2020-09-21 11:28:22 +0200

    qq

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <div id="navbar">
   <div class="container">
     <ul>
-      <li><a href="#">Home</a></li>
+      <li><a href="{{route('home')}}">Home</a></li>
       <li><a href="#">Prodotti</a></li>
       <li><a href="#">News</a></li>
     </ul>
